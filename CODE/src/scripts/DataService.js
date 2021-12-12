5cc605ea56d00d4fc8ae67e13b67a21958b7b3bd
Dejan-Stojkoski <dejan_pp2005@live.com>
2021-12-12 16:32:46 +0100

    fix: Quizzes feature fixed

--- CODE/src/scripts/DataService.js
+++ CODE/src/scripts/DataService.js
@@ -1,7 +1,7 @@
 const DataService = {
     cachedData: null,
     cachedReplyMessages: RepliesService.replies,
-    cachedQuizzes: null,
+    cachedQuizzes: QuizzesService.getQuizzesData(),
 
     //Fetch the data from JSON
     getDataAsync: async function () {
@@ -13,21 +13,6 @@ const DataService = {
         } catch (error) {
             AnimationsService.chatWindow.innerHTML =
                 `<div id="errorWrapper" class="errorWrapper"> <img src="./src/img-avatars/errorEmo.png" alt="Error Img">
-                <div class="errorText">"Oops, Something is wrong with my circuits I can't process the command"</br>Please try again later!</div>
-            </div>`;
-        }
-
-    },
-
-    //Fetches all data from the Quizzes questions json
-    fetchQuizzes: async function () {
-        try {
-            let response = await fetch(`https://raw.githubusercontent.com/sedc-codecademy/sp2021-cp10-dsc/main/JSON/Quizzes.json`);
-            let data = await response.json();
-            DataService.cachedQuizzes = await data;
-        } catch (error) {
-            QuizzesService.gamesAndQuizzesWindow.innerHTML =
-                `<div id="errorWrapper" class="errorWrapper"> <img src="./src/img-avatars/quizzesError.png" alt="Error Img">
                 <div class="errorText">"Oops, Something is wrong with my circuits I can't process the command"</br>Please try again later!</div>
             </div>`;
         }
